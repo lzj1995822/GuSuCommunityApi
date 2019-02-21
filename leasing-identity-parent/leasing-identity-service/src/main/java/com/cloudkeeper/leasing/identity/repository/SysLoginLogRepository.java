ec31d52d8de8a43831aecb9f7d2a2ac9e9140724
lxw <243485908@qq.com>
2019-02-21 15:31:40 +0800

    sysloginlog

---
+++ leasing-identity-parent/leasing-identity-service/src/main/java/com/cloudkeeper/leasing/identity/repository/SysLoginLogRepository.java
@@ -0,0 +1,14 @@
+package com.cloudkeeper.leasing.identity.repository;
+
+import com.cloudkeeper.leasing.identity.domain.SysLoginLog;
+import com.cloudkeeper.leasing.base.repository.BaseRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 类属性配置 repository
+ * @author lxw
+ */
+@Repository
+public interface SysLoginLogRepository extends BaseRepository<SysLoginLog> {
+
+}
